4265b4bc750770d09eefb5e4ac7f6901f58a11a3
lksjoshi1997 <lksjoshi1997@gmail.com>
2019-06-02 16:35:27 +0530

    image selects from gallery and converts to bitmap

--- app/src/main/java/com/example/youtu/bitmapproject/MainActivity.java
+++ app/src/main/java/com/example/youtu/bitmapproject/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.youtu.bitmapproject;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    ImageView imageView;
+    Button btn;
+    String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
+
+        initVariables();
+    }
+
+    private void initVariables() {
+        imageView = findViewById(R.id.image);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermissions();
+            }
+        });
+    }
+
+    private void checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, 10233);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 10233) {
+            if (grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                Toast.makeText(context, "Please give the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1002);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1002) {
+            if(data != null) {
+                Uri selectImage = data.getData();
+                try {
+                    Bitmap bmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectImage);
+                    imageView.setImageBitmap(bmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
 }
